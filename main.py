434cbb09cad7d87a08c0157b4a911751ed38d85e
Matthew Morrison <matthew_morrison@sfu.ca>
2023-06-21 10:11:44 -0700

    Update API and codes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import json
 from discord.ext import commands
+import discord
+import asyncio
 
 # epoch must be in the format like "1 January 2020 PST", ie, following the "%d %B %Y %Z" date format.
 
@@ -17,18 +19,23 @@ if os.path.isfile(configFile):
 else:
     print("Uh... no config file. Gonna explode now.")
 
-bot = commands.Bot(command_prefix='!', description=description)
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 bot.rocode_minute = rocode_minute
 bot.rocode_hour = rocode_hour
 bot.epoch_str = epoch
 bot.timezone_str = timezone
-bot.load_extension("rocode")
-
+#bot.load_extension("cogs.rocode")
 
 @bot.event
 async def on_ready():
     print('Logged in as', bot.user.name, bot.user.id, '\n-----')
 
 
-if __name__ == '__main__':
-    bot.run(discord_token)
+async def main():
+    async with bot:
+        await bot.load_extension('cogs.rocode')
+        await bot.start(discord_token)
+
+asyncio.run(main())
